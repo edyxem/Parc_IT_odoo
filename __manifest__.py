a5483d861eda0adc36460c611e670da787797b36
unknown <emmanueltossou75@gmail.com>
2026-06-23 00:03:57 +0000

    Probleme de manifest resolu

--- __manifest__.py
+++ __manifest__.py
@@ -1,67 +1,42 @@
-# -*- coding: utf-8 -*-
 {
-    'name': 'IT Parc',
+    'name': 'IT Parc - Gestion de parc informatique',
     'version': '18.0.1.0.0',
     'category': 'Technical',
     'summary': 'Gestion complète du parc informatique de TECHPARK CI',
-    'description': """
-        Module de gestion de parc informatique pour Odoo 18.
-        - Inventaire des équipements
-        - Affectation aux employés
-        - Suivi des interventions
-        - Gestion des contrats fournisseurs
-        - Alertes automatiques de garantie
-        - Rapports PDF et exports Excel
-        - Dashboard OWL
-    """,
-    'author': 'Tossou',
-    'website': 'https://www.techpark-ci.com',
-    'license': 'LGPL-3',
-
     'depends': [
         'base',
         'mail',
         'hr',
-        'stock',
         'contacts',
         'web',
     ],
-
+    'application': True,
     'data': [
         'security/security_groups.xml',
         'security/ir.model.access.csv',
         'security/ir_rules.xml',
-
         'data/ir_sequence.xml',
         'data/ir_cron.xml',
-        
-        'views/it_parc_menus.xml',
-        'views/it_parc_actions.xml',
-
         'views/dashboard_action.xml',
-
         'views/equipement_views.xml',
         'views/affectation_views.xml',
         'views/intervention_views.xml',
         'views/contrat_views.xml',
         'views/alerte_views.xml',
-        
-
         'wizards/wizard_reaffectation_views.xml',
         'wizards/wizard_import_csv_views.xml',
         'wizards/wizard_scan_alerte_views.xml',
         'wizards/wizard_renouvellement_views.xml',
-
         'report/report_actions.xml',
         'report/report_fiche_equipement.xml',
         'report/report_inventaire.xml',
         'report/report_maintenance.xml',
+        'views/it_parc_actions.xml',
+        'views/it_parc_menus.xml',
     ],
-
     'demo': [
         'data/it_parc_demo.xml',
     ],
-
     'assets': {
         'web.assets_backend': [
             'it_parc/static/src/components/ItParcDashboard.js',
@@ -69,8 +44,6 @@
             'it_parc/static/src/components/dashboard.scss',
         ],
     },
-
     'installable': True,
-    'application': True,
-    'auto_install': False,
+    'license': 'LGPL-3',
 }
